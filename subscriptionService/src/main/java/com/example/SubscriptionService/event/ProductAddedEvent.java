f96f1f92636001a28845d133c48aeaed2638249f
Davik <davik0516@mail.ru>
2025-02-23 16:24:10 +0300

    без поняти что это и откуда

--- subscriptionService/src/main/java/com/example/SubscriptionService/event/ProductAddedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.example.SubscriptionService.event;
-
-import lombok.Data;
-
-@Data
-public class ProductAddedEvent { // для десериализации сообщений из кафки
-    private Long productId;          // ID нового продукта
-    private Long defaultCustomerId;  // ID клиента, для которого создаётся дефолтная подписка
-}
