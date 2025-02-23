551fbc5d7fc8e7c372f2ef2177d4dda03c5eeaf8
Davik <davik0516@mail.ru>
2025-02-23 16:23:34 +0300

    Consumer

--- subscriptionService/src/main/java/com/example/SubscriptionService/eventListener/ProductEventListener.java
+++ subscriptionService/src/main/java/com/example/SubscriptionService/eventListener/ProductEventListener.java
@@ -1,26 +1,35 @@
 package com.example.SubscriptionService.eventListener;
 
-import com.example.SubscriptionService.dto.create.SubscriptionCreateDTO;
-import com.example.SubscriptionService.event.ProductAddedEvent;
+import com.example.SubscriptionService.event.ProductCreatedEvent;
 import com.example.SubscriptionService.service.SubscriptionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class ProductEventListener {
 
     private final SubscriptionService subscriptionService;
+    private final ObjectMapper objectMapper;
 
-    public ProductEventListener(SubscriptionService subscriptionService) {
+    @Autowired
+    public ProductEventListener(SubscriptionService subscriptionService, ObjectMapper objectMapper) {
         this.subscriptionService = subscriptionService;
+        this.objectMapper = objectMapper;
     }
 
-    @KafkaListener(topics = "product-added", groupId = "subscriptions-group")
-    public void handleProductAdded(ProductAddedEvent event) {
-        SubscriptionCreateDTO dto = new SubscriptionCreateDTO();
-        dto.setClientId(event.getDefaultCustomerId());
-        dto.setProductId(event.getProductId());
-        dto.setEventType("DEFAULT_EVENT"); // Дефолтное событие, например, "PRICE_CHANGE"
-        subscriptionService.createSubscription(dto);
+    @KafkaListener(topics = "product-created", groupId = "subs-group")
+    public void handleProductCreatedEvent(String message) {
+        try {
+            ProductCreatedEvent event = objectMapper.readValue(message, ProductCreatedEvent.class);
+            Long productId = event.getProductId();
+            Long customerId = event.getCustomerId();
+            subscriptionService.createDefaultSubscription(productId, customerId);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to parse product-created event: " + message, e);
+        }
     }
 }
