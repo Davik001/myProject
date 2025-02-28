874d079b5202166d5c845e60de192ccd95b24b34
Davik <davik0516@mail.ru>
2025-02-28 23:31:15 +0300

    добавил crm контейнер

--- subscriptionService/src/main/java/com/example/SubscriptionService/CrmCustomer.java
+++ subscriptionService/src/main/java/com/example/SubscriptionService/CrmCustomer.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "crm-service", url = "${crm-service.url:http://crm-service}")
+@FeignClient(name = "crm-service", url = "http://crm-service")
 public interface CrmCustomer {
 
     @GetMapping("/api/customers/{customerId}")
