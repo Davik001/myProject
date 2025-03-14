3cd3adb842c67c18aa2bb8732262e12da6182fec
Davik <davik0516@mail.ru>
2025-03-14 22:03:07 +0300

    добавил get/set, IDEA игнорирует lombok

--- subscriptionService/src/main/java/com/example/SubscriptionService/dto/alldtos/SubscriptionDTO.java
+++ subscriptionService/src/main/java/com/example/SubscriptionService/dto/alldtos/SubscriptionDTO.java
@@ -11,4 +11,36 @@ public class SubscriptionDTO {
     private Long productId;
     private String eventType;
     private LocalDateTime createdAt;
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+    public String getEventType() {
+        return eventType;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public Long getCustomerId() {
+        return customerId;
+    }
+
 }
