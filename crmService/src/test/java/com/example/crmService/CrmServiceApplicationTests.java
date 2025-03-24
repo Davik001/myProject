b160b5334d053b6cf5c28a889483572aac58ba9b
Davik <davik0516@mail.ru>
2025-03-24 22:06:09 +0300

    .

---
+++ crmService/src/test/java/com/example/crmService/CrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crmService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
