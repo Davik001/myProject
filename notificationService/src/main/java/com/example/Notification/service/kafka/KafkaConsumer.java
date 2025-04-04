75ac9a40fe27feb62cb0dad05438601db2b094e2
Davik <davik0516@mail.ru>
2025-04-04 20:20:17 +0300

    убрал лишнее

--- notificationService/src/main/java/com/example/Notification/service/kafka/KafkaConsumer.java
+++ notificationService/src/main/java/com/example/Notification/service/kafka/KafkaConsumer.java
@@ -7,27 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
+import com.example.shared.SubscriptionEvent;
 
 @Service
 public class KafkaConsumer {
 
-    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
     private final KafkaTemplate<String, SubscriptionEvent> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public KafkaConsumer(KafkaTemplate<String, SubscriptionEvent> kafkaTemplate) {
+    public KafkaConsumer(KafkaTemplate<String, SubscriptionEvent> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(topics = "${spring.kafka.topics.subscription-events}", groupId = "${spring.kafka.consumer.group-id}")
     public void readSubscription(String message) {
-        logger.info("Получено сообщение из Kafka: {}", message);
-
-        // Создаем корректный JSON-объект, а не просто строку
-        SubscriptionEvent event = new SubscriptionEvent();
-        event.setEventType("SUBSCRIPTION_CONFIRMED");
-        event.setDetails("Подписка успешно создана!");
+        try {
+            log.info("Получено сообщение из Kafka: {}", message);
+        } catch (Exception e) {
+            log.error("Ошибка при обработке события из Kafka: {}", e.getMessage(), e);
+        }
     }
 }
